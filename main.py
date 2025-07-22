743d970cbfa05ada29eceddcc25e3b84e239477c
AlexeyM01 <AlexeyM01@yandex.ru>
2025-07-22 15:20:51 +0300

    Refactor main.py file for readabiility. Added comments

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.include_router(auth_router)
 
 @app.on_event("startup")
 async def startup_event():
-    """Документация"""
+    """Функция, выполняемая при запуске приложения fastapi"""
     await init_db()
 
 
@@ -26,14 +26,14 @@ async def check_db_connection(db: AsyncSession = Depends(get_db)):
     """Функция-проверка соединения с базой данных"""
     try:
         await db.execute(select(UserModel).limit(1))
-        return HTMLResponse(status_code=200, content="Database connection is successful")
+        return HTMLResponse(status_code=200, content="Соединение с базой данных успешно")
     except Exception as e:
-        return HTMLResponse(status_code=500, content=f"Database connection failed. Error {e} occurred")
+        return HTMLResponse(status_code=500, content=f"Соединение с базой данных разорвано. Произошла ошибка {e}")
 
 
 @app.websocket("/ws/tasks/")
 async def websocket_endpoint(websocket: WebSocket):
-    """Документация"""
+    """Обрабатывает веб-сокет соединение для взаимодействия с клиентом"""
     await websocket.accept()
     while True:
         try:
@@ -42,6 +42,7 @@ async def websocket_endpoint(websocket: WebSocket):
         except Exception as e:
             await websocket.close()
             print(f"WebSocket error: {e}")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
